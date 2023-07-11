3f5cf9ff51abf9fcbff3869110196505821e8d00
fajaradhitiapratama <fajaradhitia20@gmail.com>
2023-07-11 15:59:31 +0700

    up

--- js/equipment.js
+++ js/equipment.js
@@ -19,7 +19,7 @@ var swiper = new Swiper(".slide-container", {
 });
 // marketing
 var swiper = new Swiper(".slide-content-marketing", {
-  slidesPerView: 4,
+  slidesPerView: 3,
   spaceBetween: 25,
   loop: true,
   autoplay: {
@@ -39,3 +39,34 @@ var swiper = new Swiper(".slide-content-marketing", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+//support
+// Get the DOM elements for the image carousel
+const wrapper = document.querySelector(".wrapper-support"),
+  carousel = document.querySelector(".carousel-support"),
+  images = document.querySelectorAll("img"),
+  buttons = document.querySelectorAll(".button-support");
+let imageIndex = 0,
+  intervalId;
+const autoSlide = () => {
+  intervalId = setInterval(() => slideImage(++imageIndex), 2500);
+};
+autoSlide();
+const slideImage = () => {
+  if (imageIndex > 6) {
+    imageIndex = 0;
+  } else if (imageIndex < 0) {
+    imageIndex = 6;
+  }
+
+  carousel.style.transform = `translate(-${imageIndex * 100}%)`;
+};
+const updateClick = (e) => {
+  clearInterval(intervalId);
+  imageIndex += e.target.id === "next" ? 1 : -1;
+  slideImage(imageIndex);
+  autoSlide();
+};
+buttons.forEach((button) => button.addEventListener("click", updateClick));
+wrapper.addEventListener("mouseover", () => clearInterval(intervalId));
+wrapper.addEventListener("mouseleave", autoSlide);
