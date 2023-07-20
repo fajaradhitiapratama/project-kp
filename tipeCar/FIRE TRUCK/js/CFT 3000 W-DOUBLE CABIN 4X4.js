c6d68294c67d9e3a981eaa7eec3d088cfc3c189e
fajaradhitiapratama <fajaradhitia20@gmail.com>
2023-07-20 14:58:00 +0700

    update

---
+++ tipeCar/FIRE TRUCK/js/CFT 3000 W-DOUBLE CABIN 4X4.js
@@ -0,0 +1,15 @@
+function changeImage(imageNumber) {
+  var imgChange = document.getElementById("img-change");
+
+  var newImageSource = "";
+  if (imageNumber === 1) {
+    newImageSource = "/asset/Mobil/airport crash tender/airport FT/1.webp";
+  } else if (imageNumber === 2) {
+    newImageSource = "/asset/Mobil/airport crash tender/airport FT/2.webp";
+  } else if (imageNumber === 3) {
+    newImageSource = "/asset/Mobil/airport crash tender/airport FT/3.webp";
+  } else if (imageNumber === 4) {
+    newImageSource = "/asset/Mobil/airport crash tender/airport FT/4.webp";
+  }
+  imgChange.src = newImageSource;
+}
