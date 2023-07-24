aa39bded4b3293e5e1df258baea6d6cc100c39d1
fajaradhitiapratama <fajaradhitia20@gmail.com>
2023-07-24 09:58:55 +0700

    UPDATE

--- tipeCar/RESCUE/js/RJ TOLL ROAD.js
+++ tipeCar/RESCUE/js/RJ TOLL ROAD.js
@@ -3,13 +3,17 @@ function changeImage(imageNumber) {
 
   var newImageSource = "";
   if (imageNumber === 1) {
-    newImageSource = "/asset/Mobil/airport crash tender/airport FT/1.webp";
+    newImageSource =
+      "/asset/Mobil/rescue/RESCUE/RESCUE JEEP TOLL/whatsapp_image_2022_07_11_at_8_37_40_pm-3ba12-2856_711-t598_26.webp";
   } else if (imageNumber === 2) {
-    newImageSource = "/asset/Mobil/airport crash tender/airport FT/2.webp";
+    newImageSource =
+      "/asset/Mobil/rescue/RESCUE/RESCUE JEEP TOLL/whatsapp_image_2022_07_11_at_8_37_41_pm-d98df-2856_714-t598_26.webp";
   } else if (imageNumber === 3) {
-    newImageSource = "/asset/Mobil/airport crash tender/airport FT/3.webp";
+    newImageSource =
+      "/asset/Mobil/rescue/RESCUE/RESCUE JEEP TOLL/whatsapp_image_2022_07_11_at_8_37_41_pm_2-fda3d-2856_713-t598_26.webp";
   } else if (imageNumber === 4) {
-    newImageSource = "/asset/Mobil/airport crash tender/airport FT/4.webp";
+    newImageSource =
+      "/asset/Mobil/rescue/RESCUE/RESCUE JEEP TOLL/whatsapp_image_2022_07_11_at_8_37_41_pm_1-e5099-2856_712-t598_26.webp";
   }
   imgChange.src = newImageSource;
 }
