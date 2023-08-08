25e2543fc0a2f6a9988b45759306de622c1770df
luthfivlr <138637953+luthfivlr@users.noreply.github.com>
2023-08-08 14:44:50 +0700

    update

--- tipeCar/fire ladder/js/SKYLIFT.js
+++ tipeCar/fire ladder/js/SKYLIFT.js
@@ -3,9 +3,11 @@ function changeImage(imageNumber) {
 
   var newImageSource = "";
   if (imageNumber === 1) {
-    newImageSource = "/asset/Mobil/FL/FIRE LEDDER/SKYLIFT.png";
+    newImageSource = "/asset/Mobil/FL/skylif/IMG-20210715-WA0027.jpg";
   } else if (imageNumber === 2) {
-    newImageSource = "/asset/Mobil/FL/FIRE LEDDER/SKYLIFT2.png";
+    newImageSource = "/asset/Mobil/FL/skylif/IMG-20210715-WA0031.jpg";
+  } else if (imageNumber === 3) {
+    newImageSource = "/asset/Mobil/FL/skylif/IMG-20210715-WA0026.jpg";
   }
   imgChange.src = newImageSource;
 }
